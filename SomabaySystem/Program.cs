c69aed101078ea6eed3135e62a0c8a2f98a9db73
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-01-16 22:08:27 +0200

    Complete UserName Admin and Reseption

--- SomabaySystem/Program.cs
+++ SomabaySystem/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System.Data.SqlClient;
 using HostelReservation.Classes;
+using SomabaySystem;
 namespace HostelReservation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t \t \t \t  ***** --- ***** --- ***** --- ***** Welcome to Soma Pay ***** --- ***** --- ***** --- *****");
+            //Console.WriteLine("\t \t \t \t  ***** --- ***** --- ***** --- ***** Welcome to Soma Pay ***** --- ***** --- ***** --- *****");
+            Console.WriteLine("\n\n");
+            string textToEnter ="***** --- ***** --- ***** --- ***** Welcome to Soma Pay ***** --- ***** --- ***** --- *****";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            Console.WriteLine("\n\n\n\n\n");
+
+            Admin admin = new Admin();
+            admin.AdminMethod();
         }
     }
 }
